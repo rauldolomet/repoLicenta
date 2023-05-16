88f61969b110778ec127d0543c8fdac4024b1809
rauldolomet <62563115+rauldolomet@users.noreply.github.com>
2023-05-16 04:22:00 +0300

    first commit

--- app.py
+++ app.py
@@ -4,18 +4,56 @@ import requests
 import json
 from fastapi import FastAPI
 from datetime import datetime
-
+import boto3
+import key_config as keys
 
 app = Flask(__name__)
+
+
+dynamoDB = boto3.resource('dynamodb')
+
+
+table = dynamoDB.create_table(
+    TableName='udkjfsjfsdfst1212111',
+    KeySchema=[
+        {
+            'AttributeName': 'username',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'password',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'username',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'password',
+            'AttributeType': 'S'
+        }
+    ],
+    ProvisionedThroughput={
+        
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        
+    }
+)
+
+
+print("Items in the AWS table: " + str(table.item_count))
 
 '''
 Define some basic level users.
 These users will only benefit from a restricted
 access level.
-''' 
+'''
 
 users = {
-    'raul': 'raul',
+    'raul': 'Raul1611@',
     'alex': 'alex',
     'steph': 'steph',
     'admin': 'admin'
@@ -40,21 +78,25 @@ having to manually navigate to the
 login route
 '''
 
+
 @app.route('/')
 def default():
     return redirect(url_for('login'))
 
+
 '''
-The login route lwill check for 
+The login route will check for 
 the users input to establish the level of access they
 are granted and returns a details page with either partially
 clasiified information or fully disclosed data depending on
 the user's permissions
 '''
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
+
     if request.method == 'POST':
         if request.form.get('username') in users.keys() and request.form.get('password') == users.get(request.form.get('username')):
             user_details = {
@@ -65,8 +107,16 @@ def login():
             }
             return redirect(url_for('homepage'))
         else:
-            error = "Invalid credentials - username or password not found ! "
+            error = "Invalid credentials -> Username or password not found ! "
     return render_template('login.html', error=error)
+
+
+'''
+The home route leads to the "homepage" of
+the project. This is where the user decides whether 
+they want to create a new entry in the system
+or scan for an already existing user
+'''
 
 
 @app.route('/home', methods=['GET', 'POST'])
@@ -78,17 +128,51 @@ def homepage():
     return render_template('homepage.html')
 
 
-@app.route('/scan', methods=['GET','POST'])
-def scanUsers():
-    if request.method == 'POST':
-        pass
-    return render_template('scanUsers.html')
+'''
+The first choice of the two presented in the homepage
+is te scanning one. This will search througth the 
+linked database filtering based on the keywords 
+the users enter and either return an err message
+or a(some) record(s) found
+'''
 
-@app.route('/create', methods=['GET','POST'])
+
+@app.route('/scan', methods=['GET', 'POST'])
+def scanUsers():
+    userFound = True
+    if request.method == 'POST':
+        if request.form.get('username') in users.keys() or request.form.get('username')=="":
+            return redirect(url_for('displaySelectedUser'))
+    err = "Couldn't find user"
+    return render_template('scanUsers.html', err=err)
+
+
+'''
+The other option available for the moment is
+creating a new entry in the system. This route 
+allows the user to register a new dataset into the system which 
+willa utomatically be loaded into the AWS Database
+'''
+
+
+@app.route('/create', methods=['GET', 'POST'])
 def createUsers():
     if request.method == 'POST':
         pass
     return render_template('createUser.html')
+
+
+@app.route('/results', methods=['GET', 'POST'])
+def displaySelectedUser():
+    if request.method == 'POST':
+        return redirect(url_for('getAWSData'))
+
+    return render_template('selectedUser.html')
+
+
+@app.route('/NotFoundErr')
+def userNotFoundErr():
+    return "err user not found"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
